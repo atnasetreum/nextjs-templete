831ca51bd51ac76586acfce0095d048db5816a62
Eduardo García <atnasetreum@gmail.com>
2022-10-05 11:56:54 -0500

    backup

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next2 App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
